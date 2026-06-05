020e8810dce90d963d7491e27c39b99731f467a9
Reaan06 <esteban0609ye@gmail.com>
2026-06-05 16:31:53 -0500

    feat: assemble new modular bar

--- widget/Bar.tsx
+++ widget/Bar.tsx
@@ -1,13 +1,30 @@
 import app from "ags/gtk3/app"
 import { Astal, Gtk, Gdk } from "ags/gtk3"
+import Workspaces from "./bar/Workspaces"
+import Media from "./bar/Media"
+import Clock from "./bar/Clock"
 
 export default function Bar(monitor: Gdk.Monitor) {
+    const { TOP, LEFT, RIGHT } = Astal.WindowAnchor
+
     return <window
-        name="test-bar"
-        visible={true}
+        visible
+        name={`bar-${monitor}`}
+        class="BarWindow"
+        gdkmonitor={monitor}
+        exclusivity={Astal.Exclusivity.EXCLUSIVE}
+        anchor={TOP | LEFT | RIGHT}
         application={app}>
-        <box css="background: red; min-height: 50px; min-width: 50px;">
-            <label label="TEST" />
-        </box>
+        <centerbox class="BarContainer">
+            <box halign={Gtk.Align.START}>
+                <Workspaces />
+            </box>
+            <box halign={Gtk.Align.CENTER}>
+                <Media />
+            </box>
+            <box halign={Gtk.Align.END}>
+                <Clock />
+            </box>
+        </centerbox>
     </window>
 }
